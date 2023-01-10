4ac17eb1d603d64c2213839cc484dd446cf6138a
Stephen Turner <steve.turner.1993@gmail.com>
2023-01-10 19:20:51 +1100

    Finished version 1 of Javascript. Frontend needs to be made prettier

--- app.js
+++ app.js
@@ -1,74 +1,85 @@
 const NUMBER_BUTTONS = document.querySelectorAll('.number');
 const OPERATOR_BUTTONS = document.querySelectorAll('.operator');
-const EQUALS = document.querySelectorAll('.equals');
+const EQUALS = document.querySelector('.equals');
+
+
 
 let historicalValue = document.querySelector('.history');
 let value = document.querySelector('#value');
 
 let firstNumber = '';
 let secondNunber = '';
-let currentOperator = '';
 let lastInput = '';
+let result = '';
+let currentOperator = null;
+let resetScreen = true;
 
 
 function inputHandler (e) {
-    if (value.textContent == '0') value.textContent = ''
-    if (lastInput == 'result') value.textContent = ''
+    if (resetScreen) {
+        value.textContent = ''
+        resetScreen = false;
+    
+    }
+
     value.textContent += e.target.value;
-    lastInput = 'number';
 }
 
 
 
 function operate (a, b, operator) {
-    console.log(operator)
+    console.log(operator);
 
     switch (operator) {
         case 'plus':
-          value.textContent = add(a, b);
-          lastInput = 'result'
-          firstNumber = secondNunber;
-          secondNunber = ''
-            
-        break;
-        
-        
-    
+          return add(a, b);
+        break;  
+
+        case 'subtract':
+            return subtract(a, b);
+          break; 
+
+        case 'multiply':
+            return multiply(a, b);
+         break; 
+
+        case 'divide':
+            return divide(a, b);
+        break; 
     }
+lastInput = 'result';    
+
 }
 
-
-
-
-function equalsHandler (e) {
-    lastInput = 'equals'
-}
-
-function operatorHandler (e) {
-    console.log('operator handler')
-    console.log(lastInput);
-
-    if (lastInput == 'result') {
-        currentOperator = e.target.value;
-        firstNumber = value.textContent;
-        return;
-    }; 
-
-    lastInput = 'operator';
+function setOperator (e) {
+//if current operator is empty, save the value as current operator
+if (!currentOperator) {
     currentOperator = e.target.value;
-
-    if (!firstNumber) {
-        firstNumber = value.textContent;
-        historicalValue.textContent = `${firstNumber} ${e.target.textContent}`
-        value.textContent = '0'
-    }
-
-    else {
-        secondNunber = value.textContent;
-        historicalValue.textContent += ` ${secondNunber}`
-        operate (firstNumber, secondNunber, currentOperator);
-    }
+    firstNumber = value.textContent;
+    historicalValue.textContent = `${value.textContent} ${e.target.textContent}`
+    resetScreen = true;
 }
+
+else {
+    secondNunber = value.textContent;
+    console.log(firstNumber, secondNunber, currentOperator);
+    value.textContent = operate(firstNumber, secondNunber, currentOperator);
+    historicalValue.textContent = `${value.textContent} ${e.target.textContent}`;
+    firstNumber = value.textContent;
+    currentOperator = e.target.value;
+    resetScreen = true;
+}
+}
+
+function equalsHandler () {
+    if (!firstNumber) return;
+    secondNunber = value.textContent;
+    console.log(firstNumber, secondNunber, currentOperator);
+    value.textContent = operate(firstNumber, secondNunber, currentOperator);
+    historicalValue.textContent = value.textContent;
+    currentOperator = null;
+}
+
 
 //Maths operations
 function add (a, b) {
@@ -76,18 +87,28 @@ function add (a, b) {
 }
 
 function subtract (a, b) {
-    return a-b;
+    return Number(a) - Number(b);
     }
 
 function divide (a, b) {
-    return a/b;
+    return Number(a) / Number(b);
 }
 
 function multiply (a, b) {
-    return a*b;
+    return Number(a) * Number(b);
 }
 
 
 //Event Handlers
 NUMBER_BUTTONS.forEach(item => item.addEventListener('click', inputHandler));
-OPERATOR_BUTTONS.forEach(item => item.addEventListener('click', operatorHandler));
+OPERATOR_BUTTONS.forEach(item => item.addEventListener('click', setOperator));
+EQUALS.addEventListener('click', equalsHandler);
+
+
+
+
+
+
+
+
+    
